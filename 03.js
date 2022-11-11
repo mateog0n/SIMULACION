9da053091d0bb2feee0ffcc37c9959eae5575df7
mateog0n <mateogonzalez622@gmail.com>
2022-11-11 11:57:36 -0300

    checkpoint commit

--- 03.js
+++ 03.js
@@ -1,22 +1,14 @@
-/* 
-  Importante: 
-  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
-  código dentro de las funciones ya definidas. 
-  No comentar la funcion 
-*/
 function buscarAmigo(amigos, nombre) {
-  // La funcion llamada 'buscarAmigo' recibe como argumento un array llamado 'amigos' que contiene
-  // en cada posición del arreglo un objeto que tiene como propiedades 'nombre' y 'edad'. También
-  // recibe un string llamado 'nombre'.
-  // Debe devolver el objeto cuya propiedad 'nombre' coincida con el string 'nombre' recibido por argumento.
-  // Ej:
-  //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
-  //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
-
-  // Tu código aca:
+  var amigo_encontrado;
+  amigos.forEach(amigo => { 
+    if (amigo.nombre === nombre) {
+      amigo_encontrado = amigo;
+    }
+  } ); 
+  
+  if (amigo_encontrado) { return amigo_encontrado } else { return  "Parece que no hay ningún amigo con ese nombre" };
   
 }
 
-// No modifiques nada debajo de esta linea //
 
 module.exports = buscarAmigo
